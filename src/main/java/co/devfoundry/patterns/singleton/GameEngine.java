5560c733ca292fe5529c18dec9516d082e0a9433
pawelcwik <cwiku86@gmail.com>
2017-12-10 21:12:55 +0100

    Singleton pattern - start

---
+++ src/main/java/co/devfoundry/patterns/singleton/GameEngine.java
@@ -0,0 +1,16 @@
+package co.devfoundry.patterns.singleton;
+
+public class GameEngine {
+
+    private int hp = 100;
+    private String characterName = "";
+
+    public void run() {
+        while(true) {
+            //czekamy na input gracza
+            //zmienamy stan gry
+            //renderujemy grafike
+        }
+    }
+
+}
