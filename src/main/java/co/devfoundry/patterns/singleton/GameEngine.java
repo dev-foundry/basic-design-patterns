19b349454d07a212f3028ddd5e02ab900bef7b08
pawelcwik <cwiku86@gmail.com>
2017-12-10 21:41:55 +0100

    Singleton pattern - thread-safe and serialization proof implementation

--- src/main/java/co/devfoundry/patterns/singleton/GameEngine.java
+++ src/main/java/co/devfoundry/patterns/singleton/GameEngine.java
@@ -1,6 +1,10 @@
 package co.devfoundry.patterns.singleton;
 
-public class GameEngine {
+import java.io.Serializable;
+
+public class GameEngine implements Serializable{
+
+    private static final long serialVersionUID = 23232323;
 
     private int hp = 100;
     private String characterName = "";
@@ -23,5 +27,9 @@ public class GameEngine {
 
         return instance;
 
+    }
+
+    protected Object readResolve() {
+        return getInstance();
     }
 }
