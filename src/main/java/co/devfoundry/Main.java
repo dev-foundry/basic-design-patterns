f9cc5f4100f84dc2bc92f0a3dac99cc3a1e0277e
dnowak <devfoundry01@gmail.com>
2017-12-13 20:44:47 +0100

    Builder pattern - start

--- src/main/java/co/devfoundry/Main.java
+++ src/main/java/co/devfoundry/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
-        House house2 = new House("")
+//        House house2 = new House("")
 
 
     }
