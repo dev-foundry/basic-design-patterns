de57afa29ac71ed776624c6ee1861b48e49dbfe0
dnowak <devfoundry01@gmail.com>
2018-03-24 20:11:56 +0100

    print refactor

--- src/main/java/co/devfoundry/price_calculator/pricing_strategy/RegularPrice.java
+++ src/main/java/co/devfoundry/price_calculator/pricing_strategy/RegularPrice.java
@@ -3,7 +3,7 @@ package co.devfoundry.price_calculator.pricing_strategy;
 public class RegularPrice implements PricingStrategy {
 
     public void calculatePrice(int price) {
-        System.out.println("Normalna cena: " + price);
+        System.out.println("Normalna cena: " + price + "zł");
     }
 
 }
--- src/main/java/co/devfoundry/price_calculator/pricing_strategy/SalePrice.java
+++ src/main/java/co/devfoundry/price_calculator/pricing_strategy/SalePrice.java
@@ -3,7 +3,7 @@ package co.devfoundry.price_calculator.pricing_strategy;
 public class SalePrice implements PricingStrategy {
 
     public void calculatePrice(int price) {
-        System.out.println("Przecena: " + price/2);
+        System.out.println("Przecena: " + price/2 + "zł");
     }
 
 }
