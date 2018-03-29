d17a8b0bc988aa2615e8235f17323901b5c76136
dnowak <devfoundry01@gmail.com>
2018-03-29 18:45:30 +0200

    refactor

--- src/main/java/co/devfoundry/Main.java
+++ src/main/java/co/devfoundry/Main.java
@@ -6,11 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("\nNowy posiłek: ");
         Meal riceMeal = new RiceMeal();
         riceMeal.prepareMeal();
 
         System.out.println("\nNowy posiłek: ");
+        Meal riceMealWithShrimp = new ShrimpMealDecorator(new RiceMeal());
+        riceMealWithShrimp.prepareMeal();
 
+        System.out.println("\nNowy posiłek: ");
         Meal potatoMealWithChickenAndSauce = new SauceMealDecorator(new ChickenMealDecorator(new PotatoMeal()));
         potatoMealWithChickenAndSauce.prepareMeal();
 
