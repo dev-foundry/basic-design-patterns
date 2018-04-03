fb4d264f7fe13eb13dc77af142bd5af7dc7de957
pawelcwik <cwiku86@gmail.com>
2018-04-03 22:22:15 +0200

    Adapter task - start

--- src/main/java/co/devfoundry/adapter/api/LibraryAPIv2Impl.java
+++ src/main/java/co/devfoundry/adapter/api/LibraryAPIv2Impl.java
@@ -2,20 +2,20 @@ package co.devfoundry.adapter.api;
 
 import java.time.LocalDate;
 
-public class LibraryAPIv2Impl {
+public class LibraryAPIv2Impl implements LibraryAPIv2 {
 
-    int numberOfAviableCopies(String bookTitle) {
+    public int numberOfAviableCopies(String bookTitle) {
         System.out.println("Sprawdzam dostepnosc " + bookTitle);
         return 3;
 
     }
 
-    LocalDate dueDate(String bookTitle, String firstName, String lastName, LocalDate dateOfBirth) {
+    public LocalDate dueDate(String bookTitle, String firstName, String lastName, LocalDate dateOfBirth) {
         System.out.println("Sprawdzam date zwrotu dla " + bookTitle);
         return LocalDate.now();
     }
 
-    boolean reserve(String bookTitle, String firstName, String lastName, LocalDate dateOfBirth) {
+    public boolean reserve(String bookTitle, String firstName, String lastName, LocalDate dateOfBirth) {
         System.out.println("Rezerwuje " + bookTitle);
         return true;
     }
