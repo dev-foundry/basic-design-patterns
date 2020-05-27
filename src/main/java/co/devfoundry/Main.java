b9d4b02be96595c5d237fe5a8fc713190a2da8db
Pawel Cwik <pawel@clockworkjava.pl>
2020-05-27 16:05:48 +0200

    Proxy pattern start

--- src/main/java/co/devfoundry/Main.java
+++ src/main/java/co/devfoundry/Main.java
@@ -1,9 +1,31 @@
 package co.devfoundry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        List<SavedGame> savedGames = loadSavedGames();
+        listSavedGames(savedGames);
+    }
+
+    public static List<SavedGame> loadSavedGames() {
+
+        int savedGamesCount = 20;
+        List<SavedGame> savedGames = new ArrayList<>(savedGamesCount);
+        for(int i=0;i<20;i++) {
+            savedGames.add(SavedGame.initialize());
+        }
+
+        return savedGames;
+    }
+
+    private static void listSavedGames(List<SavedGame> savedGames) {
+        for(SavedGame savedGame : savedGames) {
+            System.out.println(savedGame.getName());
+        }
     }
 
 }
--- src/main/java/co/devfoundry/SavedGame.java
+++ src/main/java/co/devfoundry/SavedGame.java
@@ -1,4 +1,38 @@
 package co.devfoundry;
 
+import static java.lang.Thread.sleep;
+
 public class SavedGame {
+
+    private String name;
+    private String gameData;
+
+    private SavedGame() {
+
+    }
+
+    public static SavedGame initialize() {
+
+        SavedGame instance = new SavedGame();
+        instance.name = String.valueOf(System.currentTimeMillis());
+        instance.gameData = loadFromStorage();
+        return instance;
+    }
+
+    public void loadGame() {
+        System.out.println("Game loaded");
+    }
+
+    private static String loadFromStorage() {
+        try {
+            sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Loaded";
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
